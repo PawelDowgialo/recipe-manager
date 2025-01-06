4a94f0ca415f950c769c78320e77bac6719f0787
PawelDowgialo <113674702+PawelDowgialo@users.noreply.github.com>
2025-01-07 00:26:49 +0100

    Update recipeRoutes.js
    abc

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -2,30 +2,27 @@ const express = require('express');
 const Recipe = require('../models/recipe');
 const router = express.Router();
 
-// GET - Pobieranie wszystkich przepisów
 router.get('/', async (req, res) => {
   try {
-    const recipes = await Recipe.find();  // Pobierz wszystkie przepisy
-    res.json(recipes);  // Zwróć przepisy w formacie JSON
+    const recipes = await Recipe.find();
+    res.json(recipes);
   } catch (err) {
     res.status(500).send('Błąd podczas pobierania przepisów');
   }
 });
 
-// POST - Dodawanie nowego przepisu
 router.post('/', async (req, res) => {
   const { name, ingredients, instructions, rating } = req.body;
   const recipe = new Recipe({ name, ingredients, instructions, rating });
 
   try {
-    const savedRecipe = await recipe.save();  // Zapisz przepis w bazie
-    res.status(201).json(savedRecipe);  // Zwróć zapisany przepis
+    const savedRecipe = await recipe.save();
+    res.status(201).json(savedRecipe);
   } catch (err) {
     res.status(400).send('Błąd podczas zapisywania przepisu');
   }
 });
 
-// PUT - Aktualizacja przepisu
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { name, ingredients, instructions, rating } = req.body;
@@ -42,13 +39,12 @@ router.put('/:id', async (req, res) => {
       return res.status(404).send('Przepis nie znaleziony');
     }
 
-    res.json(updatedRecipe);  // Zwróć zaktualizowany przepis
+    res.json(updatedRecipe);
   } catch (err) {
     res.status(400).send('Błąd podczas aktualizacji przepisu');
   }
 });
 
-// DELETE - Usuwanie przepisu
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
 
